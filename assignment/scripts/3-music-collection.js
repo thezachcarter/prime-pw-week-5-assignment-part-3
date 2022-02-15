9937b5da30583f0802b87e540bfbf9c1d9c644ec
Zach Carter <thezachcarter@gmail.com>
2022-02-15 17:47:57 -0600

    completed global variable, addToCollection function, 1 record object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ){
+  let record ={
+    title : title,
+    artist : artist,
+    yearPublished : yearPublished
+  }
+  collection.push( title, artist, yearPublished );
+  return record
+} // end addToCollection
+
+console.log( addToCollection( 'Mm..Food', 'MF DOOM', 2004  ) );
+
+console.log( collection );
