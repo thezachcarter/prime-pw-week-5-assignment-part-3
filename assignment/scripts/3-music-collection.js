89d2508b3afb60734ba192147088fb8eea7a693b
Zach Carter <thezachcarter@gmail.com>
2022-02-16 11:28:00 -0600

    added 5 more records to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -15,3 +15,23 @@ function addToCollection( title, artist, yearPublished ){
 console.log( addToCollection( 'Mm..Food', 'MF DOOM', 2004  ) );
 
 console.log( collection );
+
+console.log( addToCollection( 'Excitable Boy', 'Warren Zevon', 1978  ) );
+
+console.log( collection );
+
+console.log( addToCollection( 'Madvillainy', 'Madvillain', 2003  ) );
+
+console.log( collection );
+
+console.log( addToCollection( 'Liquid Swords', 'GZA', 1995  ) );
+
+console.log( collection );
+
+console.log( addToCollection( 'Operation: Doomsday', 'MF DOOM', 1999  ) );
+
+console.log( collection );
+
+console.log( addToCollection( 'Vaudeville Villain', 'Viktor Vaughn', 2003  ) );
+
+console.log( collection );
